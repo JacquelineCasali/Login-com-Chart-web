d3a4b61bc2b0e4722cfe8ee3025f3b26bef51824
JacquelineCasali <jcasalia@yahoo.com.br>
2023-05-23 20:42:56 -0300

    unindo front com back

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+  callAPI() {
+    // fetch("http://localhost:9000/testAPI");
+    fetch("https://api-login-com-chart.onrender.com/")
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }))
+      .catch((err) => err);
+  }
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.apiResponse}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/lib/axios.js
@@ -0,0 +1,8 @@
+// cliente http
+
+// import axios from "axios";
+
+// export const api = axios.create({
+//   baseURL: "http://localhost:9000",
+//   baseURL: "https://api-login-com-chart.onrender.com/",
+// });
